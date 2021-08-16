6d6b25b29d64640f0f68dffd6bcfbb144e88e11a
Rammah Karpous <35765926+RammahKarpous@users.noreply.github.com>
2021-08-16 14:43:00 +0100

    Update QuotesTest.php

--- tests/Feature/QuotesTest.php
+++ tests/Feature/QuotesTest.php
@@ -25,19 +25,10 @@ class getFiveQuotesTest extends TestCase
         $this->assertCount(5, $quotes);
     }
 
-    public function test_user_has_to_be_logged_in_to_enter_the_dashboard()
+    public function test_user_has_to_be_logged_in_to_see_five_random_quotes()
     {
         $response = $this->get('/dashboard');
         $response->assertStatus(302);
-    }
-
-    public function test_user_can_login()
-    {
-        $user = User::factory()->create();
-
-        $response = $this->actingAs($user)->withSession(['banned' => false])->get('/dashboard');
-
-        $response->assertStatus(200);
     }
 
     public function test_five_random_quotes_are_being_fetched_with_api()
