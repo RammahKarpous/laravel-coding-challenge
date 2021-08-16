04501f6be3ffa08b61468e59c3dff63cdce0c17d
Rammah Karpous <35765926+RammahKarpous@users.noreply.github.com>
2021-08-16 14:03:58 +0100

    Created a quotes feature test

---
+++ tests/Feature/QuotesTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class getFiveQuotesTest extends TestCase
+{
+    public function test_home_page_is_reachable()
+    {
+        $response = $this->get('/');
+        $response->assertStatus(200);
+    }
+
+    public function test_use_has_to_be_authenticated_to_enter_the_dashboard()
+    {
+        $response = $this->get('/dashboard');
+        $response->assertStatus(302);
+    }
+
+    public function test_quotes_are_being_fetched()
+    {
+        $response = $this->get('/dashboard');
+        
+    }
+}
