5ee43f264b6f19d802852903f45a29580473989b
Rammah Karpous <35765926+RammahKarpous@users.noreply.github.com>
2021-08-16 15:00:33 +0100

    Update UsersTableSeeder.php

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
@@ -13,6 +15,10 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('users')->insert([
+            'name' => 'rammah',
+            'email' => 'rammah@email.com',
+            'password' => Hash::make('password'),
+        ]);
     }
 }
