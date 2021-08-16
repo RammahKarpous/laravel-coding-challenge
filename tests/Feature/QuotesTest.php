5f1729ed8f6a030debb08eb3d8a229ebfddea594
Rammah Karpous <35765926+RammahKarpous@users.noreply.github.com>
2021-08-16 14:16:38 +0100

    Update QuotesTest.php

--- tests/Feature/QuotesTest.php
+++ tests/Feature/QuotesTest.php
@@ -2,22 +2,11 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
 
 class getFiveQuotesTest extends TestCase
 {
-
-    // use WithoutMiddleware;
-    // use RefreshDatabase;
-
-    public function test_home_page_is_reachable()
-    {
-        $response = $this->get('/');
-        $response->assertStatus(200);
-    }
 
     public function test_use_has_to_be_authenticated_to_enter_the_dashboard()
     {
@@ -25,9 +14,16 @@ class getFiveQuotesTest extends TestCase
         $response->assertStatus(302);
     }
 
-    public function test_quotes_are_being_fetched()
+    public function test_five_random_quotes_are_being_fetched()
     {
-        $response = $this->get('/dashboard');
+        $this->get('/dashboard');
 
+        $quotes = [];
+
+        for ($i=0; $i < 5; $i++) { 
+            array_push($quotes, Http::get('https://api.kanye.rest')->json());
+        }
+
+        $this->assertCount(5, $quotes);
     }
 }
