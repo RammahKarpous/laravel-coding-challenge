0a195fcdc9034b08b53561a747230a5657a9353d
Rammah Karpous <35765926+RammahKarpous@users.noreply.github.com>
2021-08-16 18:58:03 +0100

    Update PagesController.php

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -9,15 +9,7 @@ class PagesController extends Controller
 {
     public function index()
     {
-        $quotes = [];
-
-        for ($i=0; $i < 5; $i++) { 
-            array_push($quotes, Http::get('https://api.kanye.rest')->json());
-        }
-
-        return view('welcome', [
-            'quotes' => $quotes
-        ]);
+        return view('welcome');
     }
 
     public function dashboard()
