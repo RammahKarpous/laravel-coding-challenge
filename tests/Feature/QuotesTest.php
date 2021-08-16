4e33cea72f174eac0e8f8076656fc41a6d71a869
Rammah Karpous <35765926+RammahKarpous@users.noreply.github.com>
2021-08-16 14:39:15 +0100

    Update QuotesTest.php

--- tests/Feature/QuotesTest.php
+++ tests/Feature/QuotesTest.php
@@ -2,17 +2,15 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
 
 class getFiveQuotesTest extends TestCase
 {
 
-    public function test_user_has_to_be_authenticated_to_enter_the_dashboard()
-    {
-        $response = $this->get('/dashboard');
-        $response->assertStatus(302);
-    }
+    use RefreshDatabase;
 
     public function test_five_random_quotes_are_being_fetched()
     {
@@ -25,5 +23,26 @@ class getFiveQuotesTest extends TestCase
         }
 
         $this->assertCount(5, $quotes);
+    }
+
+    public function test_user_has_to_be_logged_in_to_enter_the_dashboard()
+    {
+        $response = $this->get('/dashboard');
+        $response->assertStatus(302);
+    }
+
+    public function test_user_can_login()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->withSession(['banned' => false])->get('/dashboard');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_five_random_quotes_are_being_fetched_with_api()
+    {
+        $response = $this->getJson('/api/get-quotes');
+        $response->assertStatus(200);
     }
 }
